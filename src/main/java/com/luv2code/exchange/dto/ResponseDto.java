184db877e2a8db1c9556093c25df2601c723b09f
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2024-11-27 09:59:24 +0900

    Refactor: ResponseDto에 환율 추가

--- src/main/java/com/luv2code/exchange/dto/ResponseDto.java
+++ src/main/java/com/luv2code/exchange/dto/ResponseDto.java
@@ -24,6 +24,9 @@ public class ResponseDto {
     private BigDecimal amountInKrw;
 
     @NotNull
+    private BigDecimal currencyRate;
+
+    @NotNull
     @DecimalMin(value = "0.0", inclusive = false, message = "금액은 0보다 커야 합니다.")
     @Digits(integer = 10, fraction = 2, message = "금액은 최대 10자리 정수와 소수점 2자리까지 입력 가능합니다.")
     private BigDecimal amountAfterExchange;
@@ -31,18 +34,13 @@ public class ResponseDto {
     @NotBlank
     private String status;
 
-    public ResponseDto(Long userId, Long currencyId, BigDecimal amountInKrw, BigDecimal amountAfterExchange, String status) {
-        this.userId = userId;
-        this.currencyId = currencyId;
-        this.amountInKrw = amountInKrw;
-        this.amountAfterExchange = amountAfterExchange;
-        this.status = status;
-    }
+
 
     public ResponseDto(Exchange exchange) {
         this.userId = exchange.getUser().getId();
         this.currencyId = exchange.getCurrency().getId();
         this.amountInKrw = exchange.getAmountInKrw();
+        this.currencyRate = exchange.getCurrency().getExchangeRate();
         this.amountAfterExchange = exchange.getAmountAfterExchange();
         this.status = exchange.getStatus().name();
     }
