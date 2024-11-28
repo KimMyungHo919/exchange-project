0a7cd2bbe0bf9ee14b5525c699d7c8a1a7356b58
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2024-11-28 19:53:54 +0900

    Feat: ResponseEntity 응답기능추가

--- src/main/java/com/luv2code/exchange/controller/ExchangeController.java
+++ src/main/java/com/luv2code/exchange/controller/ExchangeController.java
@@ -17,26 +17,26 @@ public class ExchangeController {
 
     // 환전요청 저장
     @PostMapping
-    public ResponseDto createExchangeRequest(@RequestBody ExchangeRequestDto dto) {
-        return exchangeService.saveExchangeRequest(dto);
+    public ResponseEntity<ResponseDto> createExchangeRequest(@RequestBody ExchangeRequestDto dto) {
+        return new ResponseEntity<>(exchangeService.saveExchangeRequest(dto), HttpStatus.CREATED);
     }
 
     // 환전 목록 조회
     @GetMapping("/{userId}")
-    public List<ResponseDto> findExchangeListByUserId(@PathVariable Long userId) {
-        return exchangeService.getExchangeList(userId);
+    public ResponseEntity<List<ResponseDto>> findExchangeListByUserId(@PathVariable Long userId) {
+        return new ResponseEntity<>(exchangeService.getExchangeList(userId), HttpStatus.OK);
     }
 
     // 사용자의 환전 요약 정보를 조회 (userId, 총 환전횟수, 총 환전금액)
     @GetMapping("/summaries/{userId}")
-    public List<ExchangeSummaryResponseDto> findExchangeSummaryList(@PathVariable Long userId) {
-        return exchangeService.getExchangeSummaryList(userId);
+    public ResponseEntity<List<ExchangeSummaryResponseDto>> findExchangeSummaryList(@PathVariable Long userId) {
+        return new ResponseEntity<>(exchangeService.getExchangeSummaryList(userId), HttpStatus.OK);
     }
 
     // 환전 상태를 취소로 업데이트
     @PutMapping("/{exchangeId}")
-    public UpdateResponseDto updateStatus(@PathVariable Long exchangeId) {
-        return exchangeService.updateStatus(exchangeId);
+    public ResponseEntity<UpdateResponseDto> updateStatus(@PathVariable Long exchangeId) {
+        return new ResponseEntity<>(exchangeService.updateStatus(exchangeId), HttpStatus.OK);
     }
 
     // 사용자를 삭제
